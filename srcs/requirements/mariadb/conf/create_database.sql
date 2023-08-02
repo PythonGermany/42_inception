f4d00e7f586a1d19c8454ccde51828d76883562c
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-08-02 03:00:14 +0200

    Connection between nginx and php-fpm works

--- srcs/requirements/mariadb/conf/create_database.sql
+++ srcs/requirements/mariadb/conf/create_database.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'wordpress_password';
+CREATE USER 'wordpress_user'@'srcs-wordpress-1' IDENTIFIED BY 'wordpress_password';
+GRANT ALL ON wordpress.* TO 'wordpress_user'@'srcs-wordpress-1';
 FLUSH PRIVILEGES;
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,4 +1,7 @@
 apt-get install -y php-fpm php-mysql curl && \
 curl -LO https://wordpress.org/latest.tar.gz && \
 tar xzvf latest.tar.gz && rm latest.tar.gz && \
-chown -R www-data:www-data wordpress
+mkdir -p /var/www/html && \
+cp -a wordpress/. /var/www/html/ && rm -rf wordpress && \
+chown -R www-data:www-data /var/www/html && \
+mv www.conf /etc/php/7.4/fpm/pool.d/
