5e264a0b859fa70bd4094725098c5bf15c6ec5ae
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-08-11 01:01:13 +0200

    Fixed redis startup implementation

--- srcs/requirements/bonus/redis/tools/redis.sh
+++ srcs/requirements/bonus/redis/tools/redis.sh
@@ -1,5 +1,5 @@
 # Install requirements for redis
-apt-get install -y redis-server supervisor && \
+apt-get install -y redis-server && \
 
 # Set up redis config
 sed -i "s/{REDIS_PASSWORD}/$REDIS_PW/g" redis.conf && \
@@ -7,5 +7,4 @@ sed -i "s/{REDIS_HOST}/$REDIS_HOST/g" redis.conf && \
 sed -i "s/{REDIS_PORT}/$REDIS_PORT/g" redis.conf && \
 
 # Move config files to their respective locations
-mv redis.conf /etc/redis/ && \
-mv supervisord.conf /etc/supervisor/conf.d/
+mv redis.conf /etc/redis/
