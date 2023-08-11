db6717763666b1210a828212e2ed41edc70f5e8b
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-08-11 21:51:16 +0200

    Added execution permissions for volume delete

--- srcs/tools/volume_delete.sh
+++ srcs/tools/volume_delete.sh
@@ -1,8 +1,8 @@
 if [ -d "/home/$USER/data/wordpress" ] || [ -d "/home/$USER/data/mariadb" ]; then
   read -p "volume_delete.sh: WARNING DANGER ZONE: Deleting your data volumes? (yes/n): " RESPONSE
   if [ "$RESPONSE" = "yes" ]; then
-    rm -rf /home/$USER/data/wordpress
-	  rm -rf /home/$USER/data/mariadb
+    sudo rm -rf /home/$USER/data/wordpress
+	  sudo rm -rf /home/$USER/data/mariadb
   else
     echo "volume_delete.sh: Aborting."
   fi
